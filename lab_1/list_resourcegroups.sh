e66028ff41eb2bc22d25eeecdd30db72e183c331
Matt Blessing <matthewblessing@gmail.com>
2020-04-27 22:07:24 -0400

    Update list_resourcegroups.sh

--- lab_1/list_resourcegroups.sh
+++ lab_1/list_resourcegroups.sh
@@ -3,3 +3,15 @@ az account show
 
 # list all resource groups
 az group list -o table
+
+# list all vms
+az vm list -o table
+
+# update azure advisor recommendations
+az advisor configuration show
+#az advisor configuration update -l 20
+
+# list azure advisor recommendations
+az advisor recommendation list --category cost
+az advisor recommendation list --category performance
+az advisor recommendation list --category HighAvailability
